378a5b6ba7a29ad47900a5ab4afb9b109de7a333
chzmo <chisomomoyo10@gmail.com>
2022-10-17 16:34:58 +0200

    Working on nav

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
-const CloseButton = document.querySelector('menu-btn');
-    
-CloseButton.AddEventListener('click', ()=> {
-    // CloseButton.classList.add('close');
-    alert('hello');
+let CloseButton = document.querySelector('#open-menu');
 
+CloseButton.addEventListener('click', (e)=> {
+    //
+    e.preventDefault();
+    let container = document.querySelector('.container')
+    let containerBefore = window.getComputedStyle(container, '::before');
+
+    container.style.setProperty('--none', 'block');
 });
+
 
