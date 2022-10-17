3c37cac5b9fac53e59d6b52934eb6d5816a0c09b
chzmo <chisomomoyo10@gmail.com>
2022-10-17 18:51:43 +0200

    Working on nav

--- js/main.js
+++ js/main.js
@@ -7,10 +7,21 @@ CloseButton.addEventListener('click', (e)=> {
     let navbar = document.querySelector('.navbar');
     //let containerBefore = window.getComputedStyle(container, '::before');
 
-    container.style.setProperty('--beforeContainer', 'block');
-    navbar.style.display = 'block';
-    CloseButton.style.zIndex = '99999';
-    CloseButton.style.backgroundImage = 'url(./../images/icon-close.svg)';
+    if(e.target.classList.contains('open')){
+        e.target.classList.remove('open');
+        e.target.classList.add('close');
+        container.style.setProperty('--beforeContainer', 'block');
+        navbar.style.display = 'block';
+        CloseButton.style.zIndex = '99999';
+        CloseButton.style.backgroundImage = 'url(./../images/icon-close.svg)';
+    } else{
+        e.target.classList.add('open');
+        navbar.style.display = 'none';
+        container.style.setProperty('--beforeContainer', 'none');
+        CloseButton.style.backgroundImage = 'url(./../images/icon-hamburger.svg)'; 
+    }
+
+    
 
     container.addEventListener('click', (e)=>{
         if(e.target.style[0] === '--beforeContainer'){
